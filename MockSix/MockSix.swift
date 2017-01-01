2972f3cc860ce1bab0c041fd35be23d36792d507
Tamas Lustyik <elveestei@gmail.com>
2017-01-01 23:11:23 +0100

    Added licensing info

--- MockSix/MockSix.swift
+++ MockSix/MockSix.swift
@@ -1,3 +1,24 @@
+//
+//  MockSix.swift
+//  MockSix
+//
+//  Copyright (c) 2017 Tamas Lustyik
+//  Portions copyright (c) 2015-2016 Daniel Burbank
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+
 /// Conform to 'Mock' to use MockSix.
 ///
 /// For a function to be hooked by MockSix successfully, it must do three things: 
