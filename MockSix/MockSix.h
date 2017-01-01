8bb3f82fd5127ba423c58f32a108080cfe2f595c
Tamas Lustyik <elveestei@gmail.com>
2017-01-01 22:37:21 +0100

    Created platform targets

--- MockSix/MockSix.h
+++ MockSix/MockSix.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017. Tamas Lustyik. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MockSix.
 FOUNDATION_EXPORT double MockSixVersionNumber;
