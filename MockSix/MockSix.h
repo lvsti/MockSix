e7d9ffe075f7c979c6276903e052aeacf43c82de
Tamas Lustyik <elveestei@gmail.com>
2017-01-01 21:58:08 +0100

    Initial Commit

---
+++ MockSix/MockSix.h
@@ -0,0 +1,19 @@
+//
+//  MockSix.h
+//  MockSix
+//
+//  Created by Tamas Lustyik on 2017. 01. 01..
+//  Copyright © 2017. Tamas Lustyik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MockSix.
+FOUNDATION_EXPORT double MockSixVersionNumber;
+
+//! Project version string for MockSix.
+FOUNDATION_EXPORT const unsigned char MockSixVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockSix/PublicHeader.h>
+
+
